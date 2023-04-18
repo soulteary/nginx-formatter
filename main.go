7eda75c219666aadfea46de3272da784ab3ad38b
soulteary <soulteary@gmail.com>
2023-04-18 22:38:19 +0800

    chore: update basic implementation

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/soulteary/nginx-formatter/internal/formatter"
 	"github.com/soulteary/nginx-formatter/internal/updater"
+	"github.com/soulteary/nginx-formatter/internal/version"
 )
 
 func main() {
-	err := updater.UpdateConfInDir(".", formatter.Formatter)
+	fmt.Printf("Nginx Formatter v%s\n\n", version.Version)
+
+	dir, err := os.Getwd()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+	}
+
+	err = updater.UpdateConfInDir(dir, formatter.Formatter)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
