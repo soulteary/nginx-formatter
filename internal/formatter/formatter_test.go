84c58eba7f5c0bb982b76ccb9a27cfb99a2d5045
soulteary <soulteary@gmail.com>
2023-04-23 15:18:01 +0800

    test: update ut for formatter.Formatter

--- internal/formatter/formatter_test.go
+++ internal/formatter/formatter_test.go
@@ -9,28 +9,32 @@ import (
 func TestFormatter(t *testing.T) {
 
 	const TestData = `
-  load_module modules/ngx_http_js_module.so;
-  
-  events { }
-  
-  http {
-        js_path "/etc/nginx/njs/";
-  
-        js_import utils.js;
-        js_import main from http/hello.js;
-  
-        server {
-              listen 80;
-  
-              location = /version {
-                  js_content utils.version;
-              }
-  
-              location / {
-                  js_content main.hello;
-              }
-        }
-  }`
+load_module modules/ngx_http_js_module.so;
+
+events {  }
+
+http {
+js_path "/etc/nginx/njs/";
+
+js_import main from http/api/set_keyval.js;
+
+keyval_zone zone=foo:10m;
+
+server {
+listen 80;
+
+location /keyval {
+js_content main.set_keyval;
+}
+location /api {
+internal;
+api write=on;
+}
+location /api/ro {
+api;
+}
+}
+}`
 
 	const TestExpected = `
 load_module modules/ngx_http_js_module.so;
@@ -38,31 +42,37 @@ load_module modules/ngx_http_js_module.so;
 events {  }
 
 http {
-  js_path "/etc/nginx/njs/";
+    js_path "/etc/nginx/njs/";
 
-  js_import utils.js;
-  js_import main from http/hello.js;
+    js_import main from http/api/set_keyval.js;
 
-  server {
-    listen 80;
+    keyval_zone zone=foo:10m;
 
-    location = /version {
-      js_content utils.version;
+    server {
+        listen 80;
+
+        location /keyval {
+            js_content main.set_keyval;
+        }
+
+        location /api {
+            internal;
+            api write=on;
+        }
+
+        location /api/ro {
+            api;
+        }
+
     }
-
-    location / {
-      js_content main.hello;
-    }
-
-  }
 }`
 
-	result, err := formatter.Formatter(TestData, 2, " ")
+	result, err := formatter.Formatter(TestData, 4, " ")
 	if err != nil {
 		t.Errorf("formatter error: %v\n", err)
 	}
 
 	if result != TestExpected {
-		t.Error("formatter result not expected")
+		t.Error("formatter result not expected", result, TestExpected)
 	}
 }
