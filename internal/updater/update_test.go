870caf51169f086aff4793a12c40e143d4960fbb
soulteary <soulteary@gmail.com>
2023-04-23 13:25:06 +0800

    test: Completely override return handling

--- internal/updater/update_test.go
+++ internal/updater/update_test.go
@@ -17,19 +17,19 @@ func TestEncodeEscapeChars(t *testing.T) {
 		},
 		{
 			input:    `Hello\tworld`,
-			expected: `Hello【\\】tworld`,
+			expected: `Hello【&】tworld`,
 		},
 		{
 			input:    `This is a test\nwith new line`,
-			expected: `This is a test【\\】nwith new line`,
+			expected: `This is a test【&】nwith new line`,
 		},
 		{
 			input:    `Some\rtext`,
-			expected: `Some【\\】rtext`,
+			expected: `Some【&】rtext`,
 		},
 		{
 			input:    `\t\s\r\n`,
-			expected: `【\\】t【\\】s【\\】r【\\】n`,
+			expected: `【&】t【&】s【&】r【&】n`,
 		},
 	}
 
@@ -51,19 +51,19 @@ func TestDecodeEscapeChars(t *testing.T) {
 			expected: "",
 		},
 		{
-			input:    `Hello【\】tworld`,
+			input:    `Hello【&】tworld`,
 			expected: `Hello\tworld`,
 		},
 		{
-			input:    `This is a test【\】nwith new line`,
+			input:    `This is a test【&】nwith new line`,
 			expected: `This is a test\nwith new line`,
 		},
 		{
-			input:    `Some【\】rtext`,
+			input:    `Some【&】rtext`,
 			expected: `Some\rtext`,
 		},
 		{
-			input:    `【\】t【\】r【\】n【\】s`,
+			input:    `【&】t【&】r【&】n【&】s`,
 			expected: `\t\r\n\s`,
 		},
 	}
@@ -81,50 +81,62 @@ func TestFixReturn(t *testing.T) {
 		input    string
 		expected string
 	}{
-		{
+		{ // empty
 			input:    "",
 			expected: "",
 		},
-		{
+		{ // add quotes to strings
 			input:    "return 200 ok;",
 			expected: "return 200 \"ok\";",
 		},
-		{
+		{ // add quotes to strings
 			input:    "return 200 $content;",
 			expected: "return 200 \"$content\";",
 		},
-		{
+		{ // keep it as it is
 			input:    "return BACKEND\n;",
 			expected: "return BACKEND;",
 		},
-		{
+		{ // keep it as it is
+			input:    "return 200 \"BACKEND B:$uri\n\";",
+			expected: "return 200 \"BACKEND B:$uri\n\";",
+		},
+		{ // keep it as it is
 			input:    "return 200;",
 			expected: "return 200;",
 		},
-		{
+		{ // trim
 			input:    "return   200   ;",
 			expected: "return 200;",
 		},
-		{
-			input:    "return \"ok\";",
+		{ // trim
+			input:    "return    \"ok\"  ; ",
 			expected: "return \"ok\";",
 		},
-		{
-			input:    "return 200 \"ok\";",
+		{ // trim
+			input:    "return   200  \"ok\" ;  ",
 			expected: "return 200 \"ok\";",
 		},
 		{
-			input:    "return 200        \"1\"    ;",
-			expected: "return 200 \"1\";",
+			input:    "return   200        \"1 1 1\"    ;",
+			expected: "return 200 \"1 1 1\";",
 		},
 		{
-			input:    "return   \"1\"    ;",
-			expected: "return \"1\";",
+			input:    "return   \"1\n\"    ;",
+			expected: "return \"1\n\";",
+		},
+		{
+			input:    "return   \"1\n  11\"    ;",
+			expected: "return \"1\n  11\";",
+		},
+		{
+			input:    "return   \"1\n\t\r  11\n\"    ;",
+			expected: "return \"1\n\t\r  11\n\";",
 		},
 	}
 
 	for _, tc := range testCases {
-		output := updater.FixVars(updater.FixReturn(updater.EncodeEscapeChars(tc.input)))
+		output := updater.FixReturn(updater.FixVars(updater.EncodeEscapeChars(tc.input)))
 		if output != tc.expected {
 			t.Errorf("Unexpected output. Input: %s, Expected: %s, Output: %s", tc.input, tc.expected, (updater.DecodeEscapeChars(output)))
 		}
--- internal/updater/updater.go
+++ internal/updater/updater.go
@@ -9,53 +9,58 @@ import (
 )
 
 func EncodeEscapeChars(s string) string {
-	return strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(s, `\t`, `【\\】t`), `\s`, `【\\】s`), `\r`, `【\\】r`), `\n`, `【\\】n`)
+	return strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(s, `\t`, `【&】t`), `\s`, `【&】s`), `\r`, `【&】r`), `\n`, `【&】n`)
 }
 
 func DecodeEscapeChars(s string) string {
-	return strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(s, `【\】t`, `\t`), `【\】s`, `\s`), `【\】r`, `\r`), `【\】n`, `\n`)
+	return strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(s, `【&】t`, `\t`), `【&】s`, `\s`), `【&】r`, `\r`), `【&】n`, `\n`)
 }
 
 func FixReturn(s string) string {
-	s = strings.TrimSpace(s)
-	if s == "" {
-		return s
-	}
 
 	var scene1 = regexp.MustCompile(`return\s+(\d+)\s+(\S+)\s*;`)
 	var scene2 = regexp.MustCompile(`return\s+(\d+)\s+"(\S+)"\s*;`)
 	var scene3 = regexp.MustCompile(`return\s+(\S+)\s*;`)
 	var scene4 = regexp.MustCompile(`return\s+"(\S+)"\s*;`)
 	var scene5 = regexp.MustCompile(`return\s+(\d+)\s*;`)
+	var scene6 = regexp.MustCompile(`return\s+"(.+)"\s*;`)
+	var scene7 = regexp.MustCompile(`return\s+(\d+)\s+"(.+)"\s*;`)
+	var scene8 = regexp.MustCompile(`return\s+(\d+)\s+"([\s|\S|\n|\r|\t]+)"\s*;`)
+	var scene9 = regexp.MustCompile(`return\s+"([\s|\S|\n|\r|\t]+)"\s*;`)
 
 	if scene1.MatchString(s) {
 		if scene2.MatchString(s) { // eg: `return 200 "ok";`
-			s = scene2.ReplaceAllString(s, "$1 \"$2\"")
+			return strings.TrimSpace(scene2.ReplaceAllString(s, "return $1 \"$2\";"))
 		} else { // eg: `return 200 $content;`
-			s = scene1.ReplaceAllString(s, "$1 \"$2\"")
+			return strings.TrimSpace(scene1.ReplaceAllString(s, "return $1 \"$2\";"))
 		}
 	} else if scene3.MatchString(s) {
-		fmt.Println("!!")
 		if scene5.MatchString(s) { // eg: `return 200;`
-			s = scene5.ReplaceAllString(s, "$1")
-		} else if scene4.MatchString(s) { // eg: `return "ok";`
-			fmt.Println("!!!!!", s)
-
-			s = scene4.ReplaceAllString(s, "\"$1\"")
+			return strings.TrimSpace(scene5.ReplaceAllString(s, "return $1;"))
+		} else if scene6.MatchString(s) { // eg: `return "ok";`
+			if scene4.MatchString(s) {
+				return strings.TrimSpace(scene4.ReplaceAllString(s, "return \"$1\";"))
+			} else {
+				return strings.TrimSpace(scene6.ReplaceAllString(s, "return \"$1\";"))
+			}
 		} else { // eg: `return BACKEND\n;`
-
 			found := scene3.FindString(s)
 			if !(strings.HasPrefix(found, `"`) && strings.HasSuffix(found, `"`)) {
-				s = scene3.ReplaceAllString(s, "$1")
+				return strings.TrimSpace(scene3.ReplaceAllString(s, "return $1;"))
 			} else {
-				s = scene3.ReplaceAllString(s, "\"$1\"")
+				return strings.TrimSpace(scene3.ReplaceAllString(s, "return \"$1\";"))
 			}
 		}
 	} else {
-		fmt.Println("---", s)
-
+		if scene7.MatchString(s) {
+			return strings.TrimSpace(scene7.ReplaceAllString(s, "return $1 \"$2\";"))
+		} else if scene8.MatchString(s) {
+			return strings.TrimSpace(scene8.ReplaceAllString(s, "return $1 \"$2\";"))
+		} else if scene9.MatchString(s) {
+			return strings.TrimSpace(scene9.ReplaceAllString(s, "return \"$1\";"))
+		}
 	}
-	return "return " + strings.TrimSpace(s) + ";"
+	return s
 }
 
 func FixVars(s string) string {
