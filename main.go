15fd21cee42faee4e35e45671a118dc3e74e772a
soulteary <soulteary@gmail.com>
2023-05-07 13:27:27 +0800

    feat: Check if the user input directory is correct

--- main.go
+++ main.go
@@ -25,7 +25,13 @@ func main() {
 		if _, err := os.Stat("/.dockerenv"); err == nil && src == "/" {
 			fmt.Println("To run using the Docker model, you need to specify a run directory other than the root directory.")
 			fmt.Println("example:")
-			fmt.Println("docker run --rm -it -v `pwd`:/app soulteary/nginx-formatter -input=/app")
+			fmt.Println("  docker run --rm -it -v `pwd`:/app soulteary/nginx-formatter -input=/app")
+			os.Exit(0)
+		}
+
+		if _, err := os.Stat(src); err != nil {
+			fmt.Println("The directory you specified does not exist, please check the path parameters and try again.")
+			fmt.Println("Input directory:", src)
 			os.Exit(0)
 		}
 
