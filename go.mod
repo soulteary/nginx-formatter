aecdbc123ab796e19cf120db77af3ac8a9029179
Su Yang <soulteary@gmail.com>
2026-08-20 22:20:36 +0800

    chore: upgrade fiber v3

--- go.mod
+++ go.mod
@@ -3,21 +3,26 @@ module github.com/soulteary/nginx-formatter
 go 1.26.0
 
 require (
-	github.com/gofiber/fiber/v2 v2.52.14
+	github.com/gofiber/fiber/v3 v3.5.0
 	github.com/spf13/cobra v1.10.2
 )
 
 require (
 	github.com/andybalholm/brotli v1.2.2 // indirect
-	github.com/clipperhouse/uax29/v2 v2.7.0 // indirect
+	github.com/gofiber/schema v1.8.4 // indirect
+	github.com/gofiber/utils/v2 v2.4.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/klauspost/compress v1.19.2 // indirect
 	github.com/mattn/go-colorable v0.1.15 // indirect
 	github.com/mattn/go-isatty v0.0.24 // indirect
-	github.com/mattn/go-runewidth v0.0.27 // indirect
-	github.com/spf13/pflag v1.0.9 // indirect
+	github.com/philhofer/fwd v1.2.0 // indirect
+	github.com/spf13/pflag v1.0.10 // indirect
+	github.com/tinylib/msgp v1.6.4 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.73.0 // indirect
+	golang.org/x/crypto v0.55.0 // indirect
+	golang.org/x/net v0.58.0 // indirect
 	golang.org/x/sys v0.47.0 // indirect
+	golang.org/x/text v0.41.0 // indirect
 )
--- internal/server/server.go
+++ internal/server/server.go
@@ -4,32 +4,32 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v3"
 )
 
 func Launch(port int, indent int, char string, fn func(s string, indent int, char string) (string, error)) error {
-	app := fiber.New(fiber.Config{DisableStartupMessage: true})
+	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
+	app.Get("/", func(c fiber.Ctx) error {
 		c.Type("html")
 		return c.Send([]byte(getDocCache()))
 	})
 
-	app.Post("/format", func(c *fiber.Ctx) error {
+	app.Post("/format", func(c fiber.Ctx) error {
 		code := c.FormValue("code")
 		updateDocCache(code, indent, char, fn)
-		return c.Redirect("/", http.StatusFound)
+		return c.Redirect().Status(http.StatusFound).To("/")
 	})
 
-	app.Get("/base.css", func(c *fiber.Ctx) error {
+	app.Get("/base.css", func(c fiber.Ctx) error {
 		c.Type("css")
 		return c.Send(CACHE_STYLESHEET)
 	})
 
-	app.Get("/base.js", func(c *fiber.Ctx) error {
+	app.Get("/base.js", func(c fiber.Ctx) error {
 		c.Type("js")
 		return c.Send(CACHE_SCRIPT)
 	})
 
-	return app.Listen(fmt.Sprintf(":%d", port))
+	return app.Listen(fmt.Sprintf(":%d", port), fiber.ListenConfig{DisableStartupMessage: true})
 }
